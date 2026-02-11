732f03c86652423ab65454413527abb71e3b4ed5
Aky <razvainnn@gmail.com>
2026-02-11 10:17:02 +0100

    changes

--- frontend/next.config.ts
+++ frontend/next.config.ts
@@ -1,9 +1,12 @@
 import type { NextConfig } from "next";
 
+const isProd = process.env.NODE_ENV === 'production'
+
+
 const nextConfig: NextConfig = {
   output: 'export',
-  // basePath: '/mosaic'  
-  // assetPrefix: '/mosaic'
+  basePath: isProd ? '/mosaic' : '',      // only for GitHub Pages
+  assetPrefix: isProd ? '/mosaic' : '',
 };
 
 // module.exports = nextConfig
